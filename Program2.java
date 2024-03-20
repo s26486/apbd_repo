f50eba1f29f17758dad79c95b80bd7affab261fb
s26486 <s26486@pjwstk.edu.pl>
2024-03-20 18:14:02 +0100

    added google login on a new branch

--- Program2.java
+++ Program2.java
@@ -1,5 +1,9 @@
 class Program2{
     public static void main(String[] args) {
         System.out.println("test");
+        System.out.println("Google login:");
+        String login = "mail";
+        String pass = "pass";
+        System.out.println(login + pass);
     }
 }
