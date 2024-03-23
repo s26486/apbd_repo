d53d843323b88feb1f18e5d0657982990ff6f2ff
s26486 <s26486@pjwstk.edu.pl>
2024-03-23 19:51:10 +0100

    Adding feature-average

---
+++ Laboratoria/Lab1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Lab2;
+
+ // See https://aka.ms/new-console-template for more information
+
+ class Program
+ {
+     public static double average(int[] integers)
+     {
+         return integers.Average();
+     }
+     
+     public static void Main(string[] args)
+     {
+         int[] array = { 1, 2, 3, 4, 5 };
+         double average = Program.average(array);
+         Console.WriteLine(average);
+         
+     }     
+ }
